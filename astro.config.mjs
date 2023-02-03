e0a9296ecae79da25893bb30ad6209d3f30a0002
John Eatmon <jeatmon@getprosource.com>
2023-02-03 10:36:43 -0800

    config: enable react integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config"
 
 import image from "@astrojs/image"
 import mdx from "@astrojs/mdx"
+import react from "@astrojs/react"
 import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
 
@@ -12,6 +13,7 @@ export default defineConfig({
 			serviceEntryPoint: "@astrojs/image/sharp",
 		}),
 		mdx(),
+		react(),
 		sitemap(),
 		tailwind({
 			config: {
