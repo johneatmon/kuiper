d4154c6b12f10d4b6f8839b7b3bf59462ef9a795
John Eatmon <jeatmon@getprosource.com>
2023-02-03 10:24:04 -0800

    feat: site fallback details

---
+++ src/data/config.ts
@@ -0,0 +1,9 @@
+type SiteProps = {
+	title: string
+	fallbackDesc: string
+}
+
+export const SITE: SiteProps = {
+	title: "Kuiper",
+	fallbackDesc: "Kuiper is an opinionated Astro starter to help you launch your websites quickly.",
+}
