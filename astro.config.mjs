014ec9dd40c1ec2575f678491fb9dc090c62de8e
John Eatmon <jeatmon@getprosource.com>
2023-02-03 10:23:33 -0800

    config: enable astro integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,23 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
+
+import image from "@astrojs/image"
+import mdx from "@astrojs/mdx"
+import sitemap from "@astrojs/sitemap"
+import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	integrations: [
+		image({
+			serviceEntryPoint: "@astrojs/image/sharp",
+		}),
+		mdx(),
+		sitemap(),
+		tailwind(),
+	],
+	vite: {
+		ssr: {
+			noExternal: ["smartypants"],
+		},
+	},
+})
