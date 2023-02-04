e0ec2c07a949fa1003d8be8b8baf048abfb5ec4d
jmaeat <jmaeatmon@icloud.com>
2023-02-04 13:45:59 -0800

    config/feat: add custom remark plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@ import { defineConfig } from "astro/config"
 
 import image from "@astrojs/image"
 import mdx from "@astrojs/mdx"
+import prefetch from "@astrojs/prefetch"
 import react from "@astrojs/react"
 import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
+
+import { remarkReadingTime } from "./remark-reading-time.mjs"
+import { remarkWidont } from "./remark-widont.mjs"
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,6 +17,10 @@ export default defineConfig({
 			serviceEntryPoint: "@astrojs/image/sharp",
 		}),
 		mdx(),
+		prefetch({
+			selector: "a[href^='https:']", // By default, only external links
+			throttle: 3,
+		}),
 		react(),
 		sitemap(),
 		tailwind({
@@ -21,10 +29,14 @@ export default defineConfig({
 			},
 		}),
 	],
+	markdown: {
+		remarkPlugins: [remarkWidont, remarkReadingTime],
+		drafts: true,
+	},
 	site: "https://example.com",
 	vite: {
 		ssr: {
-			noExternal: ["smartypants"],
+			noExternal: [/^@radix-ui\/*/, "smartypants"],
 		},
 	},
 })
